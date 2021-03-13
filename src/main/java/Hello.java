0517cea2a283bf06fe3390bc47648dc38e8b188f
Tom <dta89uct@gmail.com>
2021-03-13 10:31:24 +0700

    added profile activation via Operating system

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,8 @@ class Hello{
 	
 	public static void main(String[] args){
 		System.out.println("Hello");
+		System.out.println("Os name: " + System.getProperty("os.name"));
+		System.out.println("Os arch: " + System.getProperty("os.arch"));
+		System.out.println("Os version: " + System.getProperty("os.version"));
 	}
 }
